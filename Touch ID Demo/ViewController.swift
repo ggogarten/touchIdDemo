110ce2682bde1c28361a5b8da76b873272ad247f
George G <ggogarten@gmail.com>
2016-06-28 13:42:52 -0400

    initial commite, quick example code for TouchID

--- Touch ID Demo/ViewController.swift
+++ Touch ID Demo/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let authenticationContext = LAContext();
+        var error: NSError?
+        
+        
+        if authenticationContext.canEvaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authenticationContext.evaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, localizedReason: "Please verify your user information with Touch ID") { (success, error) in
+                
+                if success {
+                    
+                    // User has been authenticated with TouchID
+                    
+                } else {
+                    
+                    if let error = error {
+                        
+                        // There was an error with the TouchID verification
+                        
+                    } else {
+                        
+                        // User did not authenticate
+                        
+                    }
+                    
+                }
+            }
+            
+        } else {
+            
+            // No TouchID available
+            
+        }
+        
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
